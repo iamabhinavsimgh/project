31b6750f7669abf69cb953124a1e34a018502aeb
iamabhinavsimgh <142678907+iamabhinavsimgh@users.noreply.github.com>
2023-08-26 20:42:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function SwitchOn(){
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/On+Bulb.png'
+   }
+   
+   function SwitchOff(){ 
+    document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/Off+Bulb.png'
+   }
